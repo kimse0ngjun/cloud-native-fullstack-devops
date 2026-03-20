991ed5add92ae59623173b151df485b5c019c590
kimse0ngjun <tjdwns8083@gmail.com>
2026-03-20 17:43:20 +0900

    03.20 spring/react member 연동

--- spring0309/src/main/java/org/cloud/Spring0309Application.java
+++ spring0309/src/main/java/org/cloud/Spring0309Application.java
@@ -2,8 +2,10 @@ package org.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.cloud"})
 public class Spring0309Application {
 
 	public static void main(String[] args) {
--- spring0309/src/main/java/org/cloud/controller/MemberApiController.java
+++ spring0309/src/main/java/org/cloud/controller/MemberApiController.java
@@ -1,13 +1,20 @@
 package org.cloud.controller;
 
+import java.util.List;
+
 import org.cloud.config.DatabaseConfig;
 import org.cloud.dto.MemberInfoDTO;
 import org.cloud.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,33 +28,31 @@ public class MemberApiController {
 	MemberService memberService;
 	
 	@GetMapping("/list")	
-	public String openMemberList(Model model) throws Exception {
-		model.addAttribute("list", memberService.memberList());
-		return "memberList";
+	public List<MemberInfoDTO> openMemberList() throws Exception {
+		return memberService.memberList();
 	}
 	
 	@PostMapping("/write")
-	public String memberWrite(MemberInfoDTO member) throws Exception {
+	public ResponseEntity<String> memberWrite(@RequestBody MemberInfoDTO member) throws Exception {
 		memberService.writeMember(member);
-		return "redirect:/member/list";
+		return ResponseEntity.ok("success");
 	}
 	
-	@GetMapping("/detail")
-	public String memberDetail(@RequestParam("id") String id, Model model) throws Exception {
+	@GetMapping("/detail/{id}")
+	public ResponseEntity<MemberInfoDTO> memberDetail(@PathVariable("id") String id, Model model) throws Exception {
 		MemberInfoDTO member = memberService.memberDetail(id);
-		model.addAttribute("member", member);
-		return "detailMember";
+		return ResponseEntity.ok(member);
 	}
 
-	@PostMapping("/update")
-	public String memberUpdate(MemberInfoDTO member) throws Exception {
+	@PutMapping("/update")
+	public ResponseEntity<String> memberUpdate(@RequestBody MemberInfoDTO member) throws Exception {
 		memberService.updateMember(member);
-		return "redirect:/member/list";
+		return ResponseEntity.ok("success");
 	}
 	
-	@PostMapping("/delete")
-	public String memberDelete(@RequestParam("id") String id) throws Exception {
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> memberDelete(@PathVariable("id") String id) throws Exception {
 		memberService.deleteMember(id);
-		return "redirect:/member/list";
+		return ResponseEntity.ok("success");
 	}
 }
